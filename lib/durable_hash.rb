8918db66801e487916c71c4f9cdb296008756283
Francis Hwang <francis@Francis-Hwangs-MacBook-Pro.local>
2009-12-10 18:00:11 -0500

    simple read

---
+++ lib/durable_hash.rb
@@ -0,0 +1,7 @@
+module DurableHash
+  def self.included(includer)
+    def includer.[](key)
+      nil
+    end
+  end
+end
---
+++ spec/durable_hash_spec.rb
@@ -0,0 +1,38 @@
+RAILS_ENV = 'test'
+require 'rubygems'
+require 'active_record'
+require 'active_record/base'
+require File.dirname(__FILE__) + '/../lib/durable_hash'
+
+# Configure ActiveRecord
+ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + '/debug.log')
+ActiveRecord::Base.establish_connection(
+  'timeout' => 5000, 'adapter' => 'sqlite3', 'database' => 'spec/test.sqlite3', 
+  'pool' => 5
+)
+
+# Create the DB schema
+silence_stream(STDOUT) do
+  ActiveRecord::Schema.define do
+    create_table 'application_settings', :force => true do |app_setting|
+      app_setting.string 'key'
+      app_setting.string 'value'
+    end
+  end
+end
+
+# Define a sample ActiveRecord class
+class ApplicationSetting < ActiveRecord::Base
+  include DurableHash
+end
+
+# Finally some specs
+describe "ApplicationSettings that are empty" do
+  before :all do
+    ApplicationSetting.destroy_all
+  end
+
+  it 'should return nil for most everything' do
+    ApplicationSetting['foo'].should be_nil
+  end
+end
